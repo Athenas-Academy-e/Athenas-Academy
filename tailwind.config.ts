d596ad4abf8669d99a5203a727caf904d78f0767
caiofavero <caiofavero@coisalab.com>
2024-11-05 20:51:39 -0300

    responsive-settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-const {nextui} = require("@nextui-org/react");
+const { nextui } = require("@nextui-org/react");
 
 const config: Config = {
   content: [
@@ -19,6 +19,18 @@ const config: Config = {
         primary: '#020617', // Azul (como no MUI)
         secondary: '#ff6b6b', // Vermelho (como no MUI)
       },
+      screens: {
+        'smartphone': {'max': '639px'},
+        // => @media (max-width: 639px) { ... }
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      }
     },
   },
   // darkMode: "class",
