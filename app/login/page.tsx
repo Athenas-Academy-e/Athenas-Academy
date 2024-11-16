46ac76c0cd627cefd28d9634c5dff6543a44456f
caiofavero <caiofavero@coisalab.com>
2024-11-16 11:44:13 -0300

    Redirect-login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,10 +2,16 @@ import { database } from "@/database";
 import LoginForm from "../(auth)/login/LoginForms";
 import Image from "next/image";
 import { getEmpresa } from "@/queries";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
 
 export default async function LoginPage() {
   const empresa = await getEmpresa()
-  
+  const Session = await auth()
+  if(Session){
+    redirect('/dashboard')
+  }
  
   return (
   <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 h-screen">
