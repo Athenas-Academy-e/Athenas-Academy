9d0229ca4d468828d6e40137ae92cf1c241e3113
caiofavero <caiofavero@coisalab.com>
2024-12-02 17:57:19 -0300

    create-materiais

--- database.ts
+++ database.ts
@@ -1,5 +1,4 @@
-import { faGraduationCap, faHouse, faMoneyBill } from '@fortawesome/free-solid-svg-icons';
-import { light } from '@mui/material/styles/createPalette';
+import {  faBookOpen, faFileLines, faGraduationCap, faHouse, faMoneyBill } from '@fortawesome/free-solid-svg-icons';
 export const database = {
     settings: [{
         logo: '/logo-dark.png',
@@ -14,7 +13,7 @@ export const database = {
         { id:0, name: 'Pagina Principal', url: '/dashboard', icon: faHouse },
         { id:1, name: 'Financeiro', url: '/dashboard/finance', icon: faMoneyBill },
         { id:2, name: 'Frequência', url: '/dashboard/frequency', icon: faGraduationCap },
-        { id:3, name: 'Notas', url: '/dashboard/notas', icon: faHouse },
-        { id:4, name: 'Pagina Principal', url: '/dashboard', icon: faHouse },
+        { id:3, name: 'Notas', url: '/dashboard/notas', icon: faFileLines },
+        { id:4, name: 'Materiais', url: '/dashboard/materiais', icon: faBookOpen },
     ]
 }
