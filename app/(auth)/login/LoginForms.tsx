89a1f5037904652a19cd5011c7563ee053635913
caiofavero <caiofavero@coisalab.com>
2024-12-16 14:07:55 -0300

    update-login-page

--- app/(auth)/login/LoginForms.tsx
+++ app/(auth)/login/LoginForms.tsx
@@ -14,59 +14,68 @@ export default function LoginForm(empresa: any) {
     const mensagem = searchParams.get('msg');
     const formRef = useRef<HTMLFormElement>(null);
 
-    // const handleSubmit = async (e: React.FormEvent) => {
-    //     e.preventDefault()
-    //     if (formRef.current) {
-    //         formRef.current.reset();
-    //     }
-    // };
+   
     const dadosempresa = Object(empresa)
-    return (    
-        <form action={login} className="space-y-6">
-            <div>
-                <div className="mt-2">
-                    <Select label='Selecione a sua escola' name='codigo_escola' id='codigo_escola' required>
-                        {dadosempresa.empresa.map((item:any)=> (
-                            <SelectItem className='text-black' key={item.codigo} value={item.codigo} textValue={`${item.cidade} - ${item.bairro}`}>{item.cidade} - {item.bairro}</SelectItem>
-                        ))}
-                    </Select>
-                </div>
-            </div>
-            <div>
-                <div className="mt-2">
-                    <Input type='text' name='username' id='username' required autoComplete='username' label='Usuário' />
-                </div>
-            </div>
-
-            <div>
-                <div className="mt-2 ">
-                    <Input type={isVisible ? "text" : "password"} name='password' id='password' required autoComplete='password' label='Senha' endContent={
-                        <button className="focus:outline-none" type="button" onClick={toggleVisibility} aria-label="toggle password visibility">
-                            {isVisible ? (<FontAwesomeIcon icon={faEyeSlash} className="text-2xl text-default-400 pointer-events-none" />) : (<FontAwesomeIcon icon={faEye} className="text-2xl text-default-400 pointer-events-none" />)}
-                        </button>
-                    } />
-                </div>
-                <div className="flex items-center justify-end mt-2">
-                    <div className="text-sm">
-                        <a href="#" className="font-semibold text-black hover:text-[#eba93b]">
-                            Esqueceu a sua senha?
-                        </a>
+    return (
+        <>
+            <style jsx global>{`
+            input[type="number"]::-webkit-outer-spin-button,
+            input[type="number"]::-webkit-inner-spin-button {
+              -webkit-appearance: none;
+              margin: 0;
+            }
+          
+            input[type="number"] {
+              -moz-appearance: textfield;
+              appearance: none;
+            }
+          `}</style>
+            <form action={login} className="space-y-6">
+                <div>
+                    <div className="mt-2">
+                        <Select label='Selecione a sua escola' name='codigo_escola' id='codigo_escola' required>
+                            {dadosempresa.empresa.map((item: any) => (
+                                <SelectItem className='text-black' key={item.codigo} value={item.codigo} textValue={`${item.cidade} - ${item.bairro}`}>{item.cidade} - {item.bairro}</SelectItem>
+                            ))}
+                        </Select>
                     </div>
                 </div>
-            </div>
-            <div>
-                <button
-                    type="submit"
-                    className="flex w-full justify-center rounded-md bg-[#142851] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#eba93b] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#eba93b]"
-                >
-                    Entrar
-                </button>
-                {mensagem === 'CredentialsSignin' ? (
-                    <Alert severity='error'>
-                        Seu usuário ou senha estão incorretos.
-                    </Alert>
-                ) : false}
-            </div>
-        </form>
+                <div>
+                    <div className="mt-2">
+                        <Input type='number' name='username' id='username' required autoComplete='username' label='Usuário' className='appearance-none -webkit-appearance-none -moz-appearance-none' />
+                    </div>
+                </div>
+
+                <div>
+                    <div className="mt-2 ">
+                        <Input type={isVisible ? "number" : "password"} name='password' id='password' required autoComplete='password' label='Senha' endContent={
+                            <button className="focus:outline-none" type="button" onClick={toggleVisibility} aria-label="toggle password visibility">
+                                {isVisible ? (<FontAwesomeIcon icon={faEyeSlash} className="text-2xl text-default-400 pointer-events-none" />) : (<FontAwesomeIcon icon={faEye} className="text-2xl text-default-400 pointer-events-none" />)}
+                            </button>
+                        } />
+                    </div>
+                    <div className="flex items-center justify-end mt-2">
+                        <div className="text-sm">
+                            <a href="#" className="font-semibold text-black hover:text-[#eba93b]">
+                                Esqueceu a sua senha?
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <button
+                        type="submit"
+                        className="flex w-full justify-center rounded-md bg-[#142851] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#eba93b] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#eba93b]"
+                    >
+                        Entrar
+                    </button>
+                    {mensagem === 'CredentialsSignin' ? (
+                        <Alert severity='error'>
+                            Seu usuário ou senha estão incorretos.
+                        </Alert>
+                    ) : false}
+                </div>
+            </form>
+        </>
     )
 }
