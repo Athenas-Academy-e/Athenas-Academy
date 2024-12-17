20707bf9c1807a367bf72a9563e82c4dec044f1a
caiofavero <caiofavero@coisalab.com>
2024-12-17 13:53:09 -0300

    update-connectionDatabase

--- connectionDatabase.ts
+++ connectionDatabase.ts
@@ -3,12 +3,23 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const connection = mysql.createConnection({
-    host: process.env.DB_URL,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE,
-    port: Number(process.env.DB_PORT)
-})
+// Pool de conexões (reutiliza as conexões)
+let pool:any;
+
+const connection = () => {
+    if (!pool) {
+        pool = mysql.createPool({
+            host: process.env.DB_URL,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS,
+            database: process.env.DB_DATABASE,
+            port: Number(process.env.DB_PORT),
+            waitForConnections: true,
+            connectionLimit: 10,  // Número máximo de conexões simultâneas no pool
+            queueLimit: 0          // Sem limite na fila de conexões
+        });
+    }
+    return pool;
+}
 
 export default connection;
