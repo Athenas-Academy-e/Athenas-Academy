95c5967810753892fb8caae58e74956661052110
caiofavero <caiofavero@coisalab.com>
2024-12-18 13:30:37 -0300

    change-brach

--- app/(auth)/login/_actions/change_school_id.tsx
+++
@@ -1,6 +0,0 @@
-import { getEmpresa } from "@/queries";
-
-export default async function ChangeSchool(){
-  const empresa = await getEmpresa()
-  return empresa
-}
--- app/admin/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-
-export default function pageAdmin(){
-  redirect('https://athenasacademy.com.br/dkweb/admin')
-}
